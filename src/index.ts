203ff9f91808c5a3bc1f43340b7b779c4f3d93ac
Craig Simko <csimko@channelape.com>
2018-08-27 13:43:27 -0400

    Adding export for SqsMessageService

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Secrets } from './service/Secrets';
 import * as Parser from './service/parser/Parser';
 export { Parser };
 export { default as ChannelApeActionsController } from './channelape/controller/ChannelApeActionsController';
+export { default as SqsMessageService } from './service/sqsMessageService/SqsMessageService';
