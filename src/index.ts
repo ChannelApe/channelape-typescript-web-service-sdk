ca3cd4ab1c96ce4a065195832c59744c62fd283b
Craig Simko <csimko@channelape.com>
2018-08-28 09:51:26 -0400

    Adding the decompression service to the list of exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import * as Parser from './service/parser/Parser';
 export { Parser };
 export { default as ChannelApeActionsController } from './channelape/controller/ChannelApeActionsController';
 export { default as SqsMessageService } from './service/sqsMessageService/SqsMessageService';
+export { default as DecompressionService } from './service/decompressionService/DecompressionService';
