709b4a92554e622231a31edb8b8adefdebe1931f
Craig Simko <csimko@channelape.com>
2018-08-29 14:37:14 -0400

    Removing useless assignment to undefined for the health check interval

--- src/channelape/controller/ChannelApeActionsController.ts
+++ src/channelape/controller/ChannelApeActionsController.ts
@@ -9,7 +9,7 @@ const PARSE_INT_RADIX = 10;
 
 export default abstract class ChannelApeActionsController {
   protected logger: Logger;
-  private updateHealthCheckInterval: NodeJS.Timer | undefined = undefined;
+  private updateHealthCheckInterval: NodeJS.Timer | undefined;
 
   constructor(loggerName: string, protected channelApeClient: ChannelApeClient) {
     this.logger = new Logger(loggerName, Secrets.env.LOG_LEVEL);
