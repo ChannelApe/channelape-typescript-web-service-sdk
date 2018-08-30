a3b61d541caac6a859a759e11a6f9de81659207d
Craig Simko <csimko@channelape.com>
2018-08-30 11:12:19 -0400

    Removing Q from XmlParsingService

--- src/parser/service/XmlParsingService.ts
+++ src/parser/service/XmlParsingService.ts
@@ -1,28 +1,26 @@
 import { Builder, parseString } from 'xml2js';
-import * as Q from 'q';
 
 export default class XmlParsingService {
   private static readonly xmlBuilder = new Builder({ headless: true });
 
-  public static toXml(obj: any): Q.Promise<string> {
-    const deferred = Q.defer<string>();
-    try {
-      deferred.resolve(this.xmlBuilder.buildObject(obj));
-    } catch (err) {
-      deferred.reject(err.message);
-    }
-    deferred.resolve();
-    return deferred.promise;
+  public static toXml(obj: any): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      try {
+        resolve(this.xmlBuilder.buildObject(obj));
+      } catch (err) {
+        return reject(err.message);
+      }
+    });
   }
 
-  public static fromXml<T>(xml: string): Q.Promise<T> {
-    const deferred = Q.defer<T>();
-    parseString(xml, (err, result) => {
-      if (err) {
-        return deferred.reject(err);
-      }
-      deferred.resolve(result);
+  public static fromXml<T>(xml: string): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      parseString(xml, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      });
     });
-    return deferred.promise;
   }
 }
