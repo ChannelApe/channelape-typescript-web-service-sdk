82100448a129960b2bbfcb6bf9c73970e8415fef
Craig Simko <csimko@channelape.com>
2018-08-30 11:06:11 -0400

    Updating the DecompressionService to no longer rely on Q

--- src/decompression/service/DecompressionService.ts
+++ src/decompression/service/DecompressionService.ts
@@ -1,18 +1,17 @@
 import * as zlib from 'zlib';
-import * as Q from 'q';
 
 export default class DecompressionService {
-  public static decompress(value: string): Q.Promise<string> {
-    const deferredRejectionMessage = Q.defer<string>();
-    const decompressedBuffer = Buffer.from(value, 'base64');
-    zlib.unzip(decompressedBuffer, (err, result) => {
-      if (err) {
-        deferredRejectionMessage.reject(err);
-        return;
-      }
-      const decompressedValue = result.toString();
-      deferredRejectionMessage.resolve(decompressedValue);
+  public static decompress(value: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const decompressedBuffer = Buffer.from(value, 'base64');
+      zlib.unzip(decompressedBuffer, (err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        const decompressedValue = result.toString();
+        resolve(decompressedValue);
+      });
     });
-    return deferredRejectionMessage.promise;
   }
 }
