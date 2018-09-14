336170a2651f62a1ba6c025c104773095903064a
Frank Mazzarella <fmazzarella@channelape.com>
2018-09-14 15:29:22 -0400

    CA-4012 - Adding a CSV parsing service

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as Secrets } from './environment/model/Secrets';
 import * as Parser from './parser/Parser';
 export { Parser };
 export { default as ChannelApeActionsController } from './channelape/controller/ChannelApeActionsController';
+export { default as AwsCredentials } from './aws/model/AwsCredentials';
 export { default as SqsMessageService } from './aws/sqs/service/SqsMessageService';
 export { default as DecompressionService } from './decompression/service/DecompressionService';
 export { default as AdditionalFieldService } from './channelape/service/AdditionalFieldService';
