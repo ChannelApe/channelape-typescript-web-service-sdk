b3c0ec611ad5936af585316f81f2b2ee2eb792e3
Frank Mazzarella <fmazzarella@channelape.com>
2018-09-17 13:42:36 -0400

    CA-4012 - Resolving code defects

--- src/report/service/ErrorReportingService.ts
+++ src/report/service/ErrorReportingService.ts
@@ -4,11 +4,20 @@ import { Logger, LogLevel } from 'channelape-logger';
 import ErrorReport from '../model/ErrorReport';
 import AwsCredentials from '../../aws/model/AwsCredentials';
 
+const SQS_MESSAGE_GROUP_ID = 'Error_Reports';
+
 export default class ErrorReportingService {
   private readonly sqsMessageService: SqsMessageService;
   private readonly logger: Logger;
+  private readonly channelApeWebAppDomainName: string;
 
-  constructor(awsCredentials: AwsCredentials, awsErrorsQueueUrl: string, logLevel: LogLevel | string) {
+  constructor(
+    awsCredentials: AwsCredentials,
+    awsErrorsQueueUrl: string,
+    logLevel: LogLevel | string,
+    stagingEnvironment = false
+  ) {
+    this.channelApeWebAppDomainName = stagingEnvironment ? 'dev.channelape.com' : 'app.channelape.com';
     this.logger = new Logger('Error Reporting Service', logLevel);
     this.sqsMessageService = new SqsMessageService(
       awsCredentials.secretKey,
@@ -18,15 +27,15 @@ export default class ErrorReportingService {
     );
   }
 
-  public queueError(params: ErrorReport): void {
+  public queueError(errorReport: ErrorReport): void {
     const message = {
-      module: params.module,
-      channelOrderId: params.channelOrderId,
-      poNumber: params.poNumber,
-      channelApeOrder: `https://app.channelape.com/orders/${params.channelApeOrderId}`,
-      message: params.message
+      module: errorReport.module,
+      channelOrderId: errorReport.channelOrderId,
+      poNumber: errorReport.poNumber,
+      channelApeOrder: `https://${this.channelApeWebAppDomainName}/orders/${errorReport.channelApeOrderId}`,
+      message: errorReport.message
     };
-    this.sqsMessageService.sendMessage(message, 'Error_Reports')
+    this.sqsMessageService.sendMessage(message, SQS_MESSAGE_GROUP_ID)
       .catch(err => this.logger.error('Failed to queue the following error: ' +
         `${JSON.stringify(message)} because: ${err}`));
   }
--- test/report/service/ErrorReportingService.spec.ts
+++ test/report/service/ErrorReportingService.spec.ts
@@ -3,9 +3,9 @@ import { expect } from 'chai';
 import { LogLevel } from 'channelape-logger';
 
 import SqsMessageService from '../../../src/aws/sqs/service/SqsMessageService';
-import ErrorReportingService from '../../../src/errorreport/service/ErrorReportingService';
-import ErrorReport from '../../../src/errorreport/model/ErrorReport';
-import ErrorReportModule from '../../../src/errorreport/model/ErrorReportModule';
+import ErrorReportingService from '../../../src/report/service/ErrorReportingService';
+import ErrorReport from '../../../src/report/model/ErrorReport';
+import ErrorReportModule from '../../../src/report/model/ErrorReportModule';
 
 describe('ErrorReportingService', () => {
   let errorReportingService: ErrorReportingService;
@@ -18,8 +18,15 @@ describe('ErrorReportingService', () => {
     module: ErrorReportModule.ORDER,
     poNumber: 'po-number'
   };
-  const expectedMessage = {
+  const expectedProdMessage = {
     channelApeOrder: 'https://app.channelape.com/orders/12345',
+    channelOrderId: 'channel-order-id',
+    message: 'hey this order is broken',
+    module: 'Order',
+    poNumber: 'po-number'
+  };
+  const expectedDevMessage = {
+    channelApeOrder: 'https://dev.channelape.com/orders/12345',
     channelOrderId: 'channel-order-id',
     message: 'hey this order is broken',
     module: 'Order',
@@ -45,7 +52,22 @@ describe('ErrorReportingService', () => {
     'Then it will add the error to the queue', (done) => {
     errorReportingService.queueError(errorMessage);
     expect(sendMessageStub.callCount).to.equal(1);
-    expect(sendMessageStub.args[0][0]).to.deep.equal(expectedMessage);
+    expect(sendMessageStub.args[0][0]).to.deep.equal(expectedProdMessage);
+    done();
+  });
+
+  it('Given sending an error report, ' +
+    'When the error report is initialized with the staging flag set to true, ' +
+    'Then it will set the ChannelApe order link to point to dev.channelape.com', (done) => {
+    errorReportingService = new ErrorReportingService(
+      { accessKeyId: 'access-key', secretKey: 'secret-key', region: 'region' },
+      'queue-url',
+      LogLevel.ERROR,
+      true
+    );
+    errorReportingService.queueError(errorMessage);
+    expect(sendMessageStub.callCount).to.equal(1);
+    expect(sendMessageStub.args[0][0]).to.deep.equal(expectedDevMessage);
     done();
   });
 });
